d1bdb4afab1a6a6332dfae3f1c41598f7071a801
Aaron Schuyler <asky28@gmail.com>
2020-05-21 00:47:43 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -2,5 +2,13 @@ require 'net/http'
 require 'open-uri'
 require 'json'
 class GetRequester
-  
+  @URL = ""
+  def initialize(url)
+    @URL = url 
+  end
+  def parse_json
+    uri = URI.parse(@URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
 end 
